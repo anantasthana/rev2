a263abf8066ff42a23e47fca01380e7eadd7d22d
Tarun Arora <tarun.arora@contoso.com>
2019-12-04 16:06:31 +0000

    adding unit test

---
+++ myweb.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myweb.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
